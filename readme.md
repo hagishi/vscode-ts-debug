87153698a40812143c9505133c9a86afc4a36cba
hagishi <hagishi.c@gmail.com>
2019-05-27 08:05:16 +0900

    initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# VS Code TS Debug
+
+## Required
+
+- [ts-node](http://ts-node)
+- [typescript](http://typescript)
+
+## Using TS Debug
+
+- Install the **TS Debug** extension in VS Code.
+- Create a new 'program' file `*.ts`.
+- Switch to the debug viewlet and press the gear dropdown.
+- Select the debug environment "TS Debug".
---
+++ src/extension.ts
@@ -0,0 +1,35 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+  // register a configuration provider for 'mock' debug type
+  const provider = new TsDebugConfigurationProvider();
+  context.subscriptions.push(
+    vscode.debug.registerDebugConfigurationProvider('ts-node', provider)
+  );
+}
+
+export function deactivate() {
+  // nothing to do
+}
+
+class TsDebugConfigurationProvider
+  implements vscode.DebugConfigurationProvider {
+  provideDebugConfigurations(
+    folder: vscode.WorkspaceFolder | undefined,
+    token?: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.DebugConfiguration[]> {
+    return Promise.resolve([]);
+  }
+
+  resolveDebugConfiguration(
+    folder: vscode.WorkspaceFolder | undefined,
+    config: vscode.DebugConfiguration,
+    token?: vscode.CancellationToken
+  ): vscode.ProviderResult<vscode.DebugConfiguration> {
+    // if launch.json is missing or empty
+    if (!config.type && !config.request && !config.name) {
+      return null;
+    }
+    return config;
+  }
+}
